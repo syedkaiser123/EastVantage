b9bc611619ce6a140965314b6ce55ee275e02972
syedkaiser9770 <syedkaiser9770@gmail.com>
2024-02-10 15:19:56 +0530

    updated test cases location

--- BooksReviewSystem/api_models/models.py
+++ BooksReviewSystem/api_models/models.py
@@ -9,7 +9,7 @@ class Book(Base):
     __tablename__ = 'books'
 
     book_id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
+    title = Column(String, unique=True)
     author = Column(String)
     publication_year = Column(Integer)
 
